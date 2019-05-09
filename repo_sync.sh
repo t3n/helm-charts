f8692d6e7d10a3823d1ce2008e3c31b8c37b818e
Max Schmidt <m.schmidt@yeebase.com>
2019-05-09 14:50:15 +0200

    TASK: Add CircleCI

--- repo_sync.sh
+++ repo_sync.sh
@@ -1,23 +1,16 @@
-#!/bin/sh -e
+#!/bin/bash -xe
 
 mkdir repository
-cd repository
-	gsutil cp gs://yeebase-helm-charts/index.yaml .
-	cp index.yaml "$HOME/.helm/repository/local/index.yaml"
-	for dir in ../*/
-	do
-		case $dir in
-			../repository/)
-				true
-			;;
-			*)
-				helm dep build "$dir"
-				helm package "$dir"
-			;;
-		esac
-	done
-	helm repo index --url https://storage.googleapis.com/yeebase-helm-charts --merge ./index.yaml .
-	gsutil -m rsync . gs://yeebase-helm-charts
-cd .. || exit
+cd repository || exit
+gsutil cp gs://yeebase-helm-charts/index.yaml .
+cp index.yaml "$HOME/.helm/repository/local/index.yaml"
+for dir in $(git diff --name-only HEAD~1 HEAD ../*/ | awk -F'/' 'NF!=1{print $1}' | sort -u);do
+	helm dep update "../$dir"
+	helm dep build "../$dir"
+	helm package "../$dir"
+done
+helm repo index --url gs://yeebase-helm-charts --merge ./index.yaml .
+gsutil -m rsync . gs://yeebase-helm-charts
+cd ..
 ls -lh repository
 rm repository -rf
