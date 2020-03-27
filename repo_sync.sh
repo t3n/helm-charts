e1148512c7c2f1da3a1f6ce6e87f13e797593c45
Jakob Helmecke <jakob.helmecke@yeebase.com>
2020-03-27 11:10:03 +0100

    Migrate pipeline to helm3

--- repo_sync.sh
+++ repo_sync.sh
@@ -3,7 +3,7 @@
 mkdir repository
 cd repository || exit
 gsutil cp gs://t3n-helm-charts/index.yaml .
-cp index.yaml "$HOME/.helm/repository/local/index.yaml"
+cp index.yaml "$HOME/.cache/helm/repository/t3n-index.yaml"
 for dir in $(git diff --name-only HEAD~1 HEAD ../*/ | awk -F'/' 'NF!=1{print $1}' | sort -u);do
 	helm dep update "../$dir"
 	helm dep build "../$dir"
