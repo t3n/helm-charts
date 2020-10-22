462e616fe98d4eb6ba210926e27958b789a3afc2
Max Schmidt <m.schmidt@yeebase.com>
2020-10-22 11:45:31 +0200

    Use CP instead of rsync for index.yaml

--- repo_sync.sh
+++ repo_sync.sh
@@ -2,7 +2,7 @@
 
 mkdir repository
 cd repository || exit
-gsutil cp gs://t3n-helm-charts/index.yaml .
+gsutil -n cp gs://t3n-helm-charts/index.yaml .
 cp index.yaml "$HOME/.cache/helm/repository/t3n-index.yaml"
 for dir in $(git diff --name-only HEAD~1 HEAD ../*/ | awk -F'/' 'NF!=1{print $1}' | sort -u);do
 	helm dep update "../$dir"
@@ -10,7 +10,8 @@ for dir in $(git diff --name-only HEAD~1 HEAD ../*/ | awk -F'/' 'NF!=1{print $1}
 	helm package "../$dir"
 done
 helm repo index --url https://storage.googleapis.com/t3n-helm-charts --merge ./index.yaml .
-gsutil -m -h "Cache-Control:private, max-age=0, no-transform" rsync . gs://t3n-helm-charts
+gsutil -m rsync -x 'index.yaml' . gs://t3n-helm-charts
+gsutil -h "Cache-Control:private, max-age=0, no-transform" cp index.yaml gs://t3n-helm-charts/index.yaml
 cd ..
 ls -lh repository
 rm repository -rf
